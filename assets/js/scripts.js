a61b9d2ad7c4fabafda1ff3dfd0bb50c58a59027
Maria Nery <67118326+mnvnery@users.noreply.github.com>
2022-04-21 12:42:45 +0100

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	$('#close-icon').click(function(){
+		$(this).toggleClass('open');
+        $('.pages-nav').toggleClass('pages-nav-open');
+	});
+});
+
+
+$(document).on('click',function(e){
+    if(!(($(e.target).closest(".pages-nav").length > 0 ) || ($(e.target).closest("#close-icon").length > 0))){
+        $(".pages-nav").removeClass('pages-nav-open');
+        $('#close-icon').removeClass('open');
+    }
+});
